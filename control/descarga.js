faa72caf23c57ef9d5bebbd43a6a17f9b2ba3cb2
Ale Taguite <dataguite@hotmail.com>
2025-09-25 07:45:18 -0600

    correción

--- control/descarga.js
+++ control/descarga.js
@@ -1,6 +1,11 @@
 export function descarga() {
-    const div = document.body; // 👈 captura todo el body
-    window.html2canvas(div).then(canvas => {
+    const div = document.body; 
+    window.html2canvas(div, {
+        ignoreElements: (element) => {
+            // ignoramos el header y el formulario
+            return element.tagName === "HEADER" || element.tagName === "FORM";
+        }
+    }).then(canvas => {
         const enlace = document.createElement("a");
         enlace.href = canvas.toDataURL("image/png");
         enlace.download = "Lista_compras.png";
